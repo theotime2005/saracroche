1f4d4104379ee6fdbea8eb26ccb97df675fdcd16
Camille Bouvat <mail@cbouvat.com>
2025-03-23 15:00:13 +0100

    Initialize new SwiftUI project with basic structure and assets

---
+++ saracroche/saracrocheApp.swift
@@ -0,0 +1,17 @@
+//
+//  saracrocheApp.swift
+//  saracroche
+//
+//  Created by Camille Bouvat on 23/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct saracrocheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
